38599749acaf3541189890c39a5cb208fb4e76ba
dillippattnaik <dillippattnaik@ymail.com>
2012-08-26 20:26:05 +0530

    Update word-service/src/main/java/org/learn/word/service/WordService.java
    method comment

--- word-service/src/main/java/org/learn/word/service/WordService.java
+++ word-service/src/main/java/org/learn/word/service/WordService.java
@@ -7,5 +7,10 @@ package org.learn.word.service;
 
 public interface WordService 
 {
+/**
+*
+* @param number which needs to be converted
+* @return  value in british text corresponding to the input number
+*/
      public String getWord(int number);
 }
